99bda69ca228dd9d11e010e81de7982052469d8f
akash <akash12t@gmail.com>
2023-03-26 03:58:47 +0530

    c1:   plays/yum-pack.sh

--- plays/yum-pack.sh
+++ plays/yum-pack.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+ansible all -m yum_repository -a 'file=BaseOS name=EX407 description='EX407 Description' baseurl=http://content.example.com/rhel8.0/x86_64/dvd/BaseOS gpgcheck=yes gpgkey=l=http://content.example.com/rhel8.0/x86_64/dvd/RPM-GPG-KEYredhat-release enabled=yes'
+
+
+ansible all -m yum_repository -a 'file=AppStream name=EXX407 description='EXX407 Description' baseurl=http://content.example.com/rhel8.0/x86_64/dvd/AppStream gpgcheck=yes gpgkey=l=http://content.example.com/rhel8.0/x86_64/dvd/RPM-GPG-KEYredhat-release enabled=yes'
+
+
